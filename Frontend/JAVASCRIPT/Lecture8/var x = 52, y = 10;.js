e3f4a81ac4ccd6e1bcf17bc8039cea68c5d329fc
Abhishek <abhi102509@gmail.com>
2023-12-03 17:26:12 +0530

    Create var x = 52, y = 10;.js

---
+++ Frontend/JAVASCRIPT/Lecture8/var x = 52, y = 10;.js
@@ -0,0 +1,23 @@
+var x = 52, y = 10;
+x = x%y;
+
+var a= 10 , b=2;
+a = a**b;
+
+
+var x =3 == 3;
+
+
+var x =3 === '3';
+
+var x =3 !== 4;
+
+
+var x =3 > 2;
+
+
+var x = 7;
+++x;
+
+var x = "Namste", y="World";
+x= x + " " + y ;
