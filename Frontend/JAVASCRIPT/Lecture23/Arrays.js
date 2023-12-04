ec1313d800f652667fdc3e8d957edecf77fcd6fc
Abhishek <abhi102509@gmail.com>
2023-12-04 17:35:16 +0530

    Create Arrays.js

---
+++ Frontend/JAVASCRIPT/Lecture23/Arrays.js
@@ -0,0 +1,43 @@
+let heroes =[
+             ["ironman", "spiderman","thor"],
+             ["superman","wonder woman","flash"]
+            ]
+
+            for(let i=0; i<heroes.length; i++){
+                console.log(i,heroes[i], heroes[i].length);
+                for(let j=0; j<heroes[i].length; j++){
+                    console.log(`j=${j}, ${heroes[i][j]}`);
+                }
+            }
+
+
+            const heroe =
+                ["ironman", "spiderman","thor"]
+            //index of 
+            heroe.indexOf('ironman')
+           console.log( heroe.indexOf('ironman'));
+
+
+
+
+// Aman...................................................  
+
+const num = [1,2,3,4,5,6,7,8,9];
+
+//Posh
+num.push(10);
+
+//unshift
+num.unshift(78)     
+
+//pop
+num.pop()
+
+//Shift
+num.shift()
+
+
+console.log(num);
+
+         
+    
