adf720b06cce29e8e2099f067e0a15d26d245abf
Abhishek <abhi102509@gmail.com>
2023-12-17 13:54:39 +0530

    Create Datatype.js

---
+++ Frontend/JAVASCRIPT/Lecture17/Datatype.js
@@ -0,0 +1,22 @@
+//Object
+let Animal = {
+    name : "cow",
+    legs: 4
+}
+console.log(Animal)
+
+//Dot Notation
+console.log(Animal.name);
+
+
+//Bracket Notation
+console.log(Animal["legs"])
+
+
+//store string in a variable
+let  legsprop = "legs"
+console.log(Animal[legsprop])
+
+//Arrays
+
+
