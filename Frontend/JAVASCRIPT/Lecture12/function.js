4dac86d164d7b04ad81c6580545cfd8a4c8a370d
Abhishek <abhi102509@gmail.com>
2023-12-17 13:56:58 +0530

    Create function.js

---
+++ Frontend/JAVASCRIPT/Lecture12/function.js
@@ -0,0 +1,11 @@
+function cookMaggi(maggi,pani,tapeli){
+    console.log("Your maggie will be ready in "
+    + maggi * 2
+    + " minutes"
+    + " and Ingredients used are:- "
+    + maggi + " maggi "
+    + pani + " cup of water "
+    + "using " + tapeli + " pan")
+
+}
+cookMaggi( 2, 4 , 1);
