67656d162253770af3576116c07b9422fc1281c2
Abhishek <abhi102509@gmail.com>
2024-02-26 21:28:39 +0530

    Create var containsDuplicate = function(nums) {.js

---
+++ Frontend/JAVASCRIPT/Lecture8/var containsDuplicate = function(nums) {.js
@@ -0,0 +1,10 @@
+var containsDuplicate = function(nums) {
+    nums= nums.sort();
+    for(i=0;i<nums.length;i++){
+        if(nums[i]==nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+    
+};
